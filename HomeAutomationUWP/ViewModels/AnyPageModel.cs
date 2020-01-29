0ee3f83920a9c8a0a015571aaaf08a01b5fc2fdb
Oliver Piter <slovakia159@centrum.sk>
2020-01-29 11:52:31 +0100

    Added support for going back from app module.

--- HomeAutomationUWP/ViewModels/AnyPageModel.cs
+++ HomeAutomationUWP/ViewModels/AnyPageModel.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using HomeAutomationUWP.Navigation;
+using HomeAutomationUWP.Helper_classes;
 
 namespace HomeAutomationUWP.ViewModels
 {
-    public class AnyPageModel
+    public class AnyPageModel : BindableBase
     {
+        private ICommand _backButton;
+        public ICommand BackButton
+        {
+            get
+            {
+                return _backButton;
+            }
+            set
+            {
+                _backButton = value;
+                NotifyPropertyChanged("BackButton");
+            }
+        }
+
         public AnyPageModel()
         {
+            SetCommands();
+        }
+
+        private void SetCommands()
+        {
+            BackButton = new RelayCommand(Back);
+        }
+
+        private void Back(object obj)
+        {
+            Navigation.Navigation.GoBack();
         }
     }
 }
--- HomeAutomationUWP/ViewModels/MenuModel.cs
+++ HomeAutomationUWP/ViewModels/MenuModel.cs
@@ -4,12 +4,16 @@ using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using HomeAutomationUWP.Helper_classes;
 using HomeAutomationUWP.Views;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
 
 namespace HomeAutomationUWP.ViewModels
 {
     public class MenuModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private Dictionary<string, Page> _activePages = new Dictionary<string, Page>();
         public MenuModel()
         {
             SetCommands();
@@ -36,7 +40,17 @@ namespace HomeAutomationUWP.ViewModels
 
         private void ShowPoolMenu(object obj)
         {
-            Navigation.Navigation.Navigate(typeof(PoolMenu));
+            Page page;
+            if (_activePages.ContainsKey("poolMenu"))
+            {
+                page = _activePages["poolMenu"];
+            }
+            else
+            {
+                _activePages.Add("poolMenu", new PoolMenu());
+                page = _activePages["poolMenu"];
+            }
+            Navigation.Navigation.Navigate(page.GetType());
         }
 
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
